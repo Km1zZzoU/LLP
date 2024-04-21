aa0a0691a57abb2c9546b041b2c959c7291ca698
Km1zZzoU <zhovnir.ea@gmail.com>
2024-04-21 21:49:08 +0700

    add rif

---
+++ RIF.asm
@@ -0,0 +1,39 @@
+section .rodata
+
+O_RDONLY equ 0
+O_WRONLY equ 1
+O_RDWR   equ 2
+limit    equ 4096
+
+section .bss
+
+buf    resb 256
+
+section .text
+rif:
+	mov rdi,          [rsp+24]     ;name
+	mov r15,          rdi
+	xor rax,          rax
+	mov rcx,          -1
+	repne scasb                   ;find \0
+	mov byte [rdi-1], 0
+	sub rdi,          [rsp+24]    ;take len
+	mov r14,          rdi
+
+	mov r13,     rsp
+	and rsp,     -16
+	mov rax,     2        ;for syscall open
+	mov rdi,     r15      ;filename
+	mov rsi,     O_RDONLY
+
+	syscall
+
+	mov rdi,     rax      ;mov fd
+	mov rsi,     buf      ;for read
+	mov rdx,     limit    ;set count
+	mov rax,     0        ;for syscall read
+
+	syscall
+
+	mov rsp,     r13
+	ret
--- task11.asm
+++ task11.asm
@@ -1,42 +1,12 @@
 global _start
 
-section .rodata
-
-O_RDONLY equ 0
-O_WRONLY equ 1
-O_RDWR   equ 2
-limit    equ 4096
-
-section .bss
-
-buf    resb 256
-
 section .text
 %INCLUDE "printdec.asm"
+%INCLUDE "mystring.asm"
+%INCLUDE "RIF.asm"
 
 _start:
-	mov rdi,         [rsp+16]     ;name
-	mov r15,          rdi
-	xor rax,          rax
-	mov rcx,          -1
-	repne scasb                   ;find \0
-	mov byte [rdi-1], 0
-	sub rdi,          [rsp+16]    ;take len
-	mov r14,          rdi
-
-	and rsp,     -16
-	mov rax,     2        ;for syscall open
-	mov rdi,     r15      ;filename
-	mov rsi,     O_RDONLY
-
-	syscall
-
-	mov rdi,     rax      ;mov fd
-	mov rsi,     buf      ;for read
-	mov rdx,     limit    ;set count
-	mov rax,     0        ;for syscall read
-
-	syscall
+	call rif
 	push rax
 
 	mov rsi,     r15      ;namefile
